fea4c6f45ff6b7f80f3e9ec531035adeb3e74b0a
Eben <bonuzx1@gmail.com>
2024-09-23 03:24:25 +0000

    chore: Update ESLint configuration and add Footer component

--- src/app/LandingPage.tsx
+++ src/app/LandingPage.tsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import NavBar from './NavBar'
 import Main from './Main'
+import Footer from './Footer'
 type Props = {}
 
 const LandingPage = (props: Props) => {
   return (
-    <div className="bg-[url('/back1.jpg')] bg-transparent [bg-gradient-to-br] from-blue-500 via-purple-500 to-pink-500 px-20 pt-12 min-h-screen">
-      <NavBar />
-      <Main></Main>
+    <div className="bg-[url('/back1.jpg')]">
+      <div className="">
+        <NavBar />
+        <Main></Main>
+        <Footer />
+      </div>
     </div>
   )
 }
