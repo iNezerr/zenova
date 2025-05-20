f392dd65286383e720a8b8155d48ad75a24396fb
Eben <bonuzx1@gmail.com>
2025-05-20 18:51:54 +0100

    chore: Update contact phone number in Contact and Footer components

--- src/app/Footer.tsx
+++ src/app/Footer.tsx
@@ -67,7 +67,7 @@ const Footer = (props: Props) => {
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-green-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
                 </svg>
-                <a href="tel:+2348138348688" className='text-gray-300 hover:text-white'>+234-813-834-8688</a>
+                <a href="tel:+2347060502621" className='text-gray-300 hover:text-white'>+234 706 050 2621</a>
               </li>
               <li className='flex items-center gap-3'>
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-green-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
